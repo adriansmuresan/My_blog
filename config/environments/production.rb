cecc0210013b7ee00d9d3c2b925180c93c7ecf98
Adrian S MUresan <adriansmuresan@gmail.com>
2017-01-26 20:27:11 -0600

    Added sendgrid production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://fathomless-temple-99705.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENGRID_USERNAME'],
+  :password => ENV['SENGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
